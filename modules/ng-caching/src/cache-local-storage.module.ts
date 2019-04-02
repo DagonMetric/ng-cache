0012d04ca6fbd4e8db5ef027f14ab00243dc31fe
Moe Myat Zaw <mmzliveid@gmail.com>
2019-04-02 16:53:53 +0630

    Initial commit

---
+++ modules/ng-caching/src/cache-local-storage.module.ts
@@ -0,0 +1,22 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+import { NgModule } from '@angular/core';
+
+import { CacheLocalStorage } from './cache-local-storage';
+import { STORAGE } from './storage';
+
+@NgModule({
+    providers: [
+        {
+            provide: STORAGE,
+            useClass: CacheLocalStorage
+        }
+    ]
+})
+export class CacheLocalStorageModule { }
