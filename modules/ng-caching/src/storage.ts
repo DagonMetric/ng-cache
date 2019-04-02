54e581640d6303ba9ef835aec2bba4e1552adc77
Moe Myat Zaw <mmzliveid@gmail.com>
2019-04-02 16:54:45 +0630

    Initial commit

---
+++ modules/ng-caching/src/storage.ts
@@ -0,0 +1,25 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+import { InjectionToken } from '@angular/core';
+
+import { CacheItem } from './cache-item';
+
+export interface Storage {
+    readonly name: string;
+    readonly enabled: boolean;
+    readonly keys: string[];
+    setItem(key: string, value: CacheItem): boolean;
+    getItem(key: string): CacheItem | undefined;
+    removeItem(key: string): void;
+    clear(): void;
+    _getNgCacheVersion(): string | null | undefined;
+    _setNgCacheVersion(ver: string): void;
+}
+
+export const STORAGE = new InjectionToken<Storage>('Storage');
