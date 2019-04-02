36ae6f5bfef7f076f9b9e982f27489f440207840
Moe Myat Zaw <mmzliveid@gmail.com>
2019-04-02 16:54:50 +0630

    Initial commit

---
+++ modules/ng-caching/src/version.ts
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+import { Version } from '@angular/core';
+
+export const VERSION = new Version('0.0.0-PLACEHOLDER');
