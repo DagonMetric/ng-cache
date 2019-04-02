d24e6b9e30b3e98cb9056a223e4f7c774dc7af2e
Moe Myat Zaw <mmzliveid@gmail.com>
2019-04-02 16:53:43 +0630

    Initial commit

---
+++ modules/ng-caching/src/cache-item.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+export interface CacheItem {
+    data: Object;
+    hash?: string;
+    absoluteExpiration?: number | null;
+    lastAccessTime?: number | null;
+    lastRemoteCheckTime?: number | null;
+}
