51e3dad390a64b1804ae9e8dd0a6f863d6f51a26
Moe Myat Zaw <mmzliveid@gmail.com>
2019-04-02 16:53:32 +0630

    Initial commit

---
+++ modules/ng-caching/src/cache-check-result.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+export interface CacheCheckResult {
+    expired: boolean;
+    hash?: string;
+    absoluteExpiration?: number;
+}
