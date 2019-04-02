6cc050fe963796b7e0159540faa1f1129e42bfd0
Moe Myat Zaw <mmzliveid@gmail.com>
2019-04-02 16:54:19 +0630

    Initial commit

---
+++ modules/ng-caching/src/index.ts
@@ -0,0 +1,23 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+export * from './cache';
+export * from './cache.module';
+export * from './cache.service';
+export * from './cache-check-result';
+export * from './cache-entry-options';
+export * from './cache-item';
+export * from './cache-local-storage';
+export * from './cache-local-storage.module';
+export * from './cache-options';
+export * from './handle-cache-response';
+export * from './initial-cache-data';
+export * from './memory-cache';
+export * from './memory-cache.module';
+export * from './return-type';
+export * from './storage';
