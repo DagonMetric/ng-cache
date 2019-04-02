a6059a0bb6cb15fafcd0d43654a784e421ec3ce4
Moe Myat Zaw <mmzliveid@gmail.com>
2019-04-02 16:54:14 +0630

    Initial commit

---
+++ modules/ng-caching/src/handle-cache-response.ts
@@ -0,0 +1,36 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+import { HttpResponse } from '@angular/common/http';
+
+import { CacheEntryOptions } from './cache-entry-options';
+
+export function handleCacheResponse<T>(response: HttpResponse<T>,
+    cacheInfoKey: string,
+    entryOptions: CacheEntryOptions): T;
+
+export function handleCacheResponse(response: HttpResponse<any>,
+    cacheInfoKey: string,
+    entryOptions: CacheEntryOptions): any {
+    if (response.headers.has(cacheInfoKey)) {
+        const headerValue = response.headers.get(cacheInfoKey);
+        if (headerValue) {
+            const cacheInfo = JSON.parse(headerValue) as CacheEntryOptions;
+            if (cacheInfo) {
+                if (cacheInfo.hash) {
+                    entryOptions.hash = cacheInfo.hash;
+                }
+                if (cacheInfo.absoluteExpiration && typeof cacheInfo.absoluteExpiration === 'number') {
+                    entryOptions.absoluteExpiration = cacheInfo.absoluteExpiration;
+                }
+            }
+        }
+    }
+
+    return response.body;
+}
