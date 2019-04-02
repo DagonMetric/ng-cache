2f48082a3466f9b1d5a7cf46e4ad203e5f2b508b
Moe Myat Zaw <mmzliveid@gmail.com>
2019-04-02 16:54:24 +0630

    Initial commit

---
+++ modules/ng-caching/src/initial-cache-data.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+import { InjectionToken } from '@angular/core';
+
+export interface InitialCacheData {
+    [key: string]: Object;
+}
+
+export const INITIAL_CACHE_DATA = new InjectionToken<InitialCacheData>('InitialCacheData');
