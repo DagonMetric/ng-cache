43aee3bf2bfb1359870188721eee6ea1ac6e702c
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-19 10:59:35 +0630

    Update tests

--- modules/ng-cache/test/cache.service.spec.ts
+++ modules/ng-cache/test/cache.service.spec.ts
@@ -1,6 +1,3 @@
-// tslint:disable: no-floating-promises
-// tslint:disable: no-console
-
 import { TestBed } from '@angular/core/testing';
 
 import { of } from 'rxjs';
@@ -36,51 +33,51 @@ describe('CacheService', () => {
             ]
         });
 
-        cacheService = TestBed.get<CacheService>(CacheService) as CacheService;
+        cacheService = TestBed.inject<CacheService>(CacheService);
     });
 
     it("should work with 'getItem'", () => {
-        expect(cacheService.getItem<string>('key1')).toBe('value1');
-        expect(cacheService.getItem<boolean>('key2')).toEqual(false);
-        expect(cacheService.getItem<number>('key3')).toEqual(0);
-        expect(cacheService.getItem<{}>('key4')).toEqual({ a: 1, b: 2 });
+        void expect(cacheService.getItem<string>('key1')).toBe('value1');
+        void expect(cacheService.getItem<boolean>('key2')).toEqual(false);
+        void expect(cacheService.getItem<number>('key3')).toEqual(0);
+        void expect(cacheService.getItem('key4')).toEqual({ a: 1, b: 2 });
     });
 
     it("should work with 'setItem'", () => {
         cacheService.setItem('testKey', 'testValue');
-        expect(cacheService.getItem<string>('testKey')).toBe('testValue');
+        void expect(cacheService.getItem<string>('testKey')).toBe('testValue');
     });
 
     it("should work with 'removeItem'", () => {
         cacheService.removeItem('key1');
-        expect(cacheService.getItem<string>('key1')).toBeFalsy();
+        void expect(cacheService.getItem<string>('key1')).toBeFalsy();
     });
 
     it("should work with 'clear'", () => {
         cacheService.clear();
-        expect(cacheService.getItem<string>('key1')).toBeFalsy();
+        void expect(cacheService.getItem<string>('key1')).toBeFalsy();
     });
 
     it("should work with 'getOrSetSync'", () => {
         const result = cacheService.getOrSetSync<string>('testKey', () => {
             return 'testValue';
         });
-        expect(result).toBe('testValue');
+        void expect(result).toBe('testValue');
     });
 
     it("should work with 'getOrSetPromise'", async () => {
-        const result = await cacheService.getOrSetPromise<string>('testKey', async () => {
-            return 'testValue';
-        });
-        expect(result).toBe('testValue');
+        const result = await cacheService.getOrSetPromise<string>('testKey', async () => Promise.resolve('testValue'));
+        void expect(result).toBe('testValue');
     });
 
     it("should work with 'getOrSet'", (done: DoneFn) => {
-        cacheService.getOrSet('testKey', () => {
-            return of('testValue');
-        }).subscribe(result => {
-            expect(result).toBe('testValue');
-            done();
-        });
+        cacheService
+            .getOrSet('testKey', () => {
+                return of('testValue');
+            })
+            .subscribe((result) => {
+                void expect(result).toBe('testValue');
+                done();
+            });
     });
 });
