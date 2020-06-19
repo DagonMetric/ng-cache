e8068349030d14e0d5b8dacadf5e2e84d725df16
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-19 10:53:40 +0630

    Update logging-api.ts

--- modules/ng-cache/src/logging-api.ts
+++ modules/ng-cache/src/logging-api.ts
@@ -2,8 +2,6 @@
  * The minimal logging interface.
  */
 export interface LoggingApi {
-    // tslint:disable-next-line: no-any
-    debug(message: string, optionalParam?: any): void;
-    // tslint:disable-next-line: no-any
-    error(message: string | Error, optionalParam?: any): void;
+    debug(message: string, optionalParam?: unknown): void;
+    error(message: string | Error, optionalParam?: unknown): void;
 }
